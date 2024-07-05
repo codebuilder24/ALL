f6481de40764ff01c40ebdd96eb2894efb0aca79
Code_Builder <codebuilder24@gmail.com>
2024-07-05 23:49:29 +0000

    Starting the second one

---
+++ Second.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class Second {
+    public static void main(String[] args) {
+        Scanner inputScanner = new Scanner (System.in);
+        inputScanner.close();
+    }
+}
