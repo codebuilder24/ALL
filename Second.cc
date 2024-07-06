70de6d6425d7f65815828de31c499decf2a065fd
Code_Builder <codebuilder24@gmail.com>
2024-07-06 03:24:27 +0000

    Almost half-way with the c++ code

---
+++ Second.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main (){
+    cout <<"This Code Is Used To Compute For The Net Pay Of Employees In A Particular Company"<<endl;
+    double grossPay , netPay , incomeTax, nationalLevy, districtTax, payMent;
+    // Starting the Main Code 
+    int hours;
+    string gender ;
+    cout<<"Please enter the gender of ther person"<<endl;
+    cin>>gender;
+    cout<<"Please, enter the hours worked by the person:";
+    cin>>hours;
+    //Writing a nexted if statement 
+
+
+    return 0;
+}
--- Second.java
+++ Second.java
@@ -3,13 +3,13 @@ public class Second {
     public static void main(String[] args) {
         Scanner inputScanner = new Scanner (System.in);
         System.out.println("This code Is Used To Compute For The Net Pay Of Employees In A Particular Company.\n");
-        double grossPay , netPay, incomeTax, nationlLevy, districtTax,payMent;
+        double grossPay , netPay, incomeTax, nationalLevy, districtTax,payMent;
         //Starting The Main Code
         System.out.println("Please enter the gender of the person:");
         String gender = inputScanner.nextLine();
         System.out.println("Please , enter the hours worked by the person: ");
         int hours = inputScanner.nextInt();
-        //Starting A while Loop
+        //Starting a nested if statement 
         if (hours<=40){
             System.out.println("It's regular Hours");
             if (gender.equals("male")) {
@@ -34,9 +34,9 @@ public class Second {
                 payMent = children*10 +(children-3)*20;
             }
             incomeTax = 0.15*grossPay;
-            nationlLevy = 0.025*grossPay;
+            nationalLevy = 0.025*grossPay;
             districtTax = 0.01*grossPay;
-            netPay = grossPay - (incomeTax+nationlLevy+districtTax +payMent);
+            netPay = grossPay - (incomeTax+nationalLevy+districtTax +payMent);
 
             System.out.println("The net pay of the employee is: " +netPay);
 
