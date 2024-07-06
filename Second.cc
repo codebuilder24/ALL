c057af96cc1440019a1e9a7c82b32888c1737b28
Code_Builder <codebuilder24@gmail.com>
2024-07-06 03:37:33 +0000

    Adding The Code That Talks About The Number Of Children

--- Second.cc
+++ Second.cc
@@ -11,6 +11,22 @@ int main (){
     cout<<"Please, enter the hours worked by the person:";
     cin>>hours;
     //Writing a nexted if statement 
+    if (hours<=40) {
+        cout<<"It's regular Hours"<<endl;
+        if(gender =="male") {
+            grossPay = 500*hours;
+        } else {
+            grossPay = 550*hours;
+        }
+    } else {
+        cout<<"It is an overtime"<<endl;
+        if (gender =="male") {
+            grossPay = 1.5*500*hours;
+        } else {
+            grossPay = 1.5*550*hours;
+        }
+    }
+    //Writing a code for the number of children
 
 
     return 0;
