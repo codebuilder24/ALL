f9f08e0a0a947da30f009667f0a39ec570c650c4
Code_Builder <codebuilder24@gmail.com>
2024-07-07 09:21:17 +0000

    Done for now

--- Third.cc
+++ Third.cc
@@ -1,26 +1,31 @@
-#include<iostream>
+#include <iostream>
 #include <vector>
 using namespace std;
-int main () {
-    int i, arraySize,num;
-    int numArray;
-    cout<<"Enter The Number Of Elements You Want To Store In Your Array:"<<endl;
-    cin>>arraySize;
-    vector<int>numArray(arraySize);
 
-    cout<<"Please,Enter " << arraySize << "numbers";
-    for(i=0;i<arraySize; i++) {
-        cin>>numArray[i];
+int main() {
+    int arraySize;
+    cout << "Enter the number of elements you want to store: ";
+    cin >> arraySize;
+
+    vector<int> numArray(arraySize); // Declare the vector with the specified size
+
+    cout << "Enter " << arraySize << " numbers: ";
+    for (int i = 0; i < arraySize; ++i) {
+        cin >> numArray[i];
     }
-    cout<<"Please , Enter the number whose last position you want to check :"<<endl;
-    cin>>num;
-    for ( i = arraySize-1; i >=0 ; i--)
-    {
-        if(num == numArray[i]) {
-            cout<<i;
+
+    cout << "Please, enter the number whose position you want to check: ";
+    int num;
+    cin >> num;
+
+    // Find the last occurrence of the number
+    for (int i = (9); i >= 0; i--) {
+        if (num == numArray[i]) {
+            cout << "The position of the last occurrence is in the index " << i 
+                 << " and that's the " << (i + 1) << " position." << endl;
             break;
         }
     }
-    
+
     return 0;
 }
