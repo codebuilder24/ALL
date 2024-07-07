6dab2dca863c30d63ddc3c48e9508b49308cf060
Code_Builder <codebuilder24@gmail.com>
2024-07-07 23:11:57 +0000

    This is the main starting point

---
+++ Fourth.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Fourth {
+    public static void main (String[] args){
+        //A code that will be used to calculate the mean and median after being given inputs from the user
+        Scanner inputScanner  = new Scanner (System.in);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        inputScanner.close();
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 #First--Used for swapping two variables 
 #Second -- Used to create on which is used to Pay workers
 #Third -- This is used to find the position of numbers. So it basically finds the position of the occurance of a given number
+#This code is used to calculate or find for mean and medians
