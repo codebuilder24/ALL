e13ed14635d85794e2001c23f849aa40c139bb17
Code_Builder <codebuilder24@gmail.com>
2024-07-08 01:46:44 +0000

    Done with mean

--- Fourth.java
+++ Fourth.java
@@ -2,10 +2,11 @@ import java.util.Scanner;
 public class Fourth {
     public static void main (String[] args){
         //A code that will be used to calculate the mean and median after being given inputs from the user
+        double sum = 0;
         Scanner inputScanner  = new Scanner (System.in);
         System.out.println("Please enter the number of Elements you wantto have inside your array");
         int num = inputScanner.nextInt();
-        int mainArray[] = new int [num];
+        double mainArray[] = new double [num];
         System.out.println("Please, enter "+num+"values in decreasing order");
         for(int i = 0; i<num ; i++) {
             mainArray[i] = inputScanner.nextInt();
@@ -19,21 +20,13 @@ public class Fourth {
         }else {
             System.out.println("Let's move on to the next code");
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        //The formula for mean is sum of all the data inside the array divided by the total number of elements you have inside your array
+        //Calculating the sum
+        for(int i = 0 ; i<num ;i++) {
+            sum = sum + mainArray[i];
+       }
+       double mean  = sum/num;
+       System.out.println("The mean is:"+mean);
 
 
 
