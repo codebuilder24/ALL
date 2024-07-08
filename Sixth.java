651527fa30690a41dc3d4585e6785d6b1a33854a
Code_Builder <codebuilder24@gmail.com>
2024-07-08 17:09:04 +0000

    Almost Done with the code

--- Sixth.java
+++ Sixth.java
@@ -3,13 +3,25 @@ import java.util.Scanner;
 public class Sixth {
     public static void main(String[] args){
         //A code that counts the number of times a particular digit appears in an integer
-        int i = 0;
+        int i = 1;
         int count = 0;
+        int sum;
+        int useNum;
         Scanner inputScanner = new Scanner(System.in);
         System.out.println("Write the number for which you want to check if other digits can enter:");
         int integer = inputScanner.nextInt();
-        for (;i<=integer; i++) {
+        useNum = integer;
+        while (i<=integer) {
             //Subtract and count. When it enters into negative , stop and give the previous value that will be left before subtracting and getting negative
+            for(;integer-i >= 0;) {
+                sum = integer - i;
+                count = count+1;
+                integer = sum;
+            }
+            System.out.println("The number of count is "+count+"for integer "+i+"The last mumber is :" +(integer));
+            integer = useNum;
+            count = 0;
+            i++;
         }
 
 
