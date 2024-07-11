20ebd232e4c2f64e3c32b3843ae08793ddc426b4
Code_Builder <codebuilder24@gmail.com>
2024-07-11 20:43:00 +0000

    Done with the code. It tells you if a number is prime

--- Seventh.java
+++ Seventh.java
@@ -2,9 +2,19 @@ import java.util.Scanner;
 public class Seventh {
     public static void main (String[] args) {
         Scanner inputScanner = new Scanner (System.in);
+        int count = 0;
+        int i;
         System.out.println("Please, enter the number you want to check if it's a prime");
         int number = inputScanner.nextInt();
-
+        for (i = 2; i<=number;i++) {
+            if (number%i==0) {
+                count = count +1;
+            }
+            
+        }
+        if(count ==1) {
+            System.out.println("It is prime");
+        }
 
 
 
