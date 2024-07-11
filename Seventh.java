8a90a0a6d17818cd10868c23b2a660f8dbea007e
Code_Builder <codebuilder24@gmail.com>
2024-07-11 20:10:37 +0000

    This stage, the user inputs the number

--- Seventh.java
+++ Seventh.java
@@ -2,5 +2,12 @@ import java.util.Scanner;
 public class Seventh {
     public static void main (String[] args) {
         Scanner inputScanner = new Scanner (System.in);
+        System.out.println("Please, enter the number you want to check if it's a prime");
+        int number = inputScanner.nextInt();
+
+
+
+
+        inputScanner.close();
     }
 }
