8bfc0115766f84b55764fe52752898cab53087e6
Code_Builder <codebuilder24@gmail.com>
2024-07-12 12:21:05 +0000

    Almost done with the code. Creating a function that will calculate the factorial of a number

--- Factorial.java
+++ Factorial.java
@@ -1,14 +1,27 @@
 import java.util.Scanner;
 public class Factorial {
     public static void main (String[] args) {
+        int i;
         //This code is used to find the factorial of a number
         Scanner inputScanner = new Scanner(System.in);
         System.out.println("Please enter the factorial whose number you want to find:");
         int number = inputScanner.nextInt();
+        while (number>=0) { {
+            for (i = 0;i<=number;i++) {
+                number -=1;
+            
+            }
+        }
+            
+        }
 
 
 
         inputScanner.close();
+    }
+
+    public static void even  () {
+        //Crafting a code that solves for the factorial of a number
     }
     
 }
