8e772f753316027f5236fc4551feb9f4ffba55e6
Code_Builder <codebuilder24@gmail.com>
2024-07-12 11:59:59 +0000

    This is the starting of the code

---
+++ Factorial.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Factorial {
+    public static void main (String[] args) {
+        //This code is used to find the factorial of a number
+        Scanner inputScanner = new Scanner(System.in);
+        System.out.println("Please enter the factorial whose number you want to find:");
+        int number = inputScanner.nextInt();
+
+
+
+        inputScanner.close();
+    }
+    
+}
