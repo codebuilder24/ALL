7bbb72a66abfe04952fd6b7d18dabbb8b3ba3b00
Code_Builder <codebuilder24@gmail.com>
2024-07-12 12:50:35 +0000

    Encountered an error , I will debug and continue later

--- Factorial.java
+++ Factorial.java
@@ -2,13 +2,18 @@ import java.util.Scanner;
 public class Factorial {
     public static void main (String[] args) {
         int i;
+        int real;
         //This code is used to find the factorial of a number
         Scanner inputScanner = new Scanner(System.in);
         System.out.println("Please enter the factorial whose number you want to find:");
         int number = inputScanner.nextInt();
         while (number>=0) { {
             for (i = 0;i<=number;i++) {
-                number -=1;
+                 real = number - 1;
+                
+                 if (number ==sub(real)) {
+                System.out.println(real);
+                 }
             
             }
         }
@@ -20,8 +25,15 @@ public class Factorial {
         inputScanner.close();
     }
 
-    public static void even  () {
+    public static int sub (int real ) {
         //Crafting a code that solves for the factorial of a number
+        int y;
+        int factorial;
+        for (y=0;y<=real;y++) {
+             factorial  = (real-y);
+
+        }
+        return (factorial);
     }
     
 }
