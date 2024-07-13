eb7c161f5b082a4162007a0d2dfa672bd91c0e3e
Code_Builder <codebuilder24@gmail.com>
2024-07-13 21:48:53 +0000

    I will work on the prime twin later

--- TwinPrime.java
+++ TwinPrime.java
@@ -8,6 +8,26 @@ public class TwinPrime {
     System.out.println("Please, enter the integer below which you want to check the win primes");
     int number = inputScanner.nextInt();
     for(prime=0; prime<=number;prime++) {
+        if (isPrime(prime) == isPrime(prime+2) ) {
+            System.out.print("It is a prime twin"+prime+""+ (prime+2));
+        }
         
     }
+    inputScanner.close();
+    }
+
+
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
 }
