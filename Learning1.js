3ae5428f014516663a87019ceeb6e961bdf1d0bc
Code_Builder <codebuilder24@gmail.com>
2024-07-21 22:44:10 +0000

    Encapsulation added

--- Learning1.js
+++ Learning1.js
@@ -58,3 +58,17 @@ for (let key in school1) {
     console.log(key)
 }
 console.log(school1.getFunction())
+
+
+
+// Using a constructor
+function person(first_name, last_name) {
+	this.first_name = first_name;
+	this.last_name = last_name;
+}
+// Creating new instances of person object
+let person1 = new person('Mukul', 'Latiyan');
+let person2 = new person('Rahul', 'Avasthi');
+
+console.log(person1.first_name);
+console.log(`${person2.first_name} ${person2.last_name}`);
