f1691be3caaf7687b049819fedf409518eec9fd2
Code_Builder <codebuilder24@gmail.com>
2024-07-21 22:52:57 +0000

    This is for abstraction

--- Learning1.js
+++ Learning1.js
@@ -72,3 +72,47 @@ let person2 = new person('Rahul', 'Avasthi');
 
 console.log(person1.first_name);
 console.log(`${person2.first_name} ${person2.last_name}`);
+
+{
+// Encapsulation example
+class person {
+	constructor(name, id) {
+		this.name = name;
+		this.id = id;
+	}
+	add_Address(add) {
+		this.add = add;
+	}
+	getDetails() {
+		console.log(`Name is ${this.name},
+		Address is: ${this.add}`);
+	}
+}
+
+let person1 = new person('Mukul', 21);
+person1.add_Address('Delhi');
+person1.getDetails();
+}
+{
+
+// Abstraction example, 
+//By defining the variables in a special way, we add abstractions to them . In this case , only some of the messages can be accessed
+function person(fname, lname) {
+	let firstname = fname;
+	let lastname = lname;
+
+	let getDetails_noaccess = function () {
+		return (`First name is: ${firstname} Last 
+			name is: ${lastname}`);
+	}
+
+	this.getDetails_access = function () {
+		return (`First name is: ${firstname}, Last 
+			name is: ${lastname}`);
+	}
+}
+let person1 = new person('Mukul', 'Latiyan');
+console.log(person1.firstname);
+console.log(person1.getDetails_noaccess);
+console.log(person1.getDetails_access());
+}
